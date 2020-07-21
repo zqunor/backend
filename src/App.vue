a4a17779b4b336e80b947bad3950f5051cb81e52
qingshan <zqunor@foxmail.com>
2020-07-21 23:03:22 +0800

    增加静态文件，样式、图片、logo

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/css/main.css";
+  @import "./assets/css/color-dark.css"; /*深色主题*/
+  /*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
 </style>
